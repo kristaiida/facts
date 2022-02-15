00ea5fbc8048c8a90efa1e36ddd742d68a6ceabd
kristaiida <92725476+kristaiida@users.noreply.github.com>
2022-02-15 13:03:01 +0200

    Simple app using API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const URL = 'https://cat-fact.herokuapp.com/facts';
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [facts, setFacts] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      setError(null);
+      setIsLoaded(true);
+      setFacts(response.data);
+    }).catch (error => {
+      alert(error);
+    });
+  }, [])
+
+  if (error) {
+    return <p>{error.message}</p>
+  } else if (!isLoaded) {
+    return <p>Loading...</p>
+  } else {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cat Facts</h1>
+      {facts.map(fact =>(
+        <div className='facts'>
+          <p>{fact.text}</p>
+        </div>
+      ))}
     </div>
   );
+}
 }
 
 export default App;
